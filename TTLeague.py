48ca678cae86e821232e831b26e8f94a9c3defc8
Jens Krause <snej@esuark.de>
2017-03-23 14:30:14 +0100

    adaptions for server data models

--- TTLeague.py
+++ TTLeague.py
@@ -1,4 +1,5 @@
-
+import time
+import json
 
 class TTLeague:
   def __init__(self, setCount=3):
@@ -8,6 +9,7 @@ class TTLeague:
     return str(self.__dict__)
 
 class Match:
+
   def __init__(self, player1, player2):
       self.games = []
       self.player1 = player1
@@ -15,17 +17,35 @@ class Match:
 
   def addGame(self, game):
       self.games.append(game)
-  
+ 
+  # data format for each match data: { w: 'Horst', l: 'Steffen Krause', diff: 2, date: 1490262281602 }
+  def getMatchDatasAsJson(self):
+      data = []
+      homePoints = 0
+      guestPoints = 0
+      for g in self.games:
+         if g.home > g.guest:
+            w = self.player1.name
+            l = self.player2.name
+            diff = g.home -g.guest
+         else:
+            w = self.player2.name
+            l = self.player1.name
+            diff = g.guest - g.home
+         print('home: {:d}, guest: {:d}'.format(homePoints, guestPoints))
+         data.append({'w': w, 'l': l, 'diff': diff, 'timestamp': g.timestamp})
+      return json.dumps(data)
+         
+ 
   def __str__(self):
-    games_str = ''
-    for g in self.games:
-      games_str += str(g)
+    games_str = json.dumps([g.__dict__ for g in self.games]) 
     return "Match: {} vs {}; Sets: {}".format(str(self.player1), str(self.player2), games_str)
 
 class Game:
   def __init__(self, home=0, guest=0):
     self.home = home
     self.guest = guest
+    self.timestamp = int(time.time()*1000)
 
   def __str__(self):
     return str(self.__dict__)
--- simulator.py
+++ simulator.py
@@ -1,4 +1,5 @@
 from TTLeague import Match, Game, Player
+from time import sleep
 import requests
 import json
 
@@ -8,8 +9,13 @@ p2 = Player("e142df52", "Jens")
 
 m = Match(p1, p2)
 g1 = Game(14, 12)
-g2 = Game(11,9)
+sleep(2)
+g2 = Game(5, 11)
+sleep(1)
+g3 = Game(11, 9)
 m.addGame(g1)
 m.addGame(g2)
+m.addGame(g3)
 
 print(str(m))
+print(m.getMatchDatasAsJson())
