dc2b2663b6ed42e5035e33fef6ed3b9620d9eb5a
thedarkman <snej@esuark.de>
2017-03-26 11:35:39 +0000

    entering points and show them on display via USB attached Keypad

--- terminal.py
+++ terminal.py
@@ -14,6 +14,7 @@ from TTLeague import Player, Match, Game
 
 from Adafruit_CharLCD import Adafruit_CharLCD
 
+from evdev import InputDevice, list_devices, categorize, ecodes, KeyEvent
 socketIO = None
 players = []
 
@@ -33,6 +34,8 @@ config.update({'lcd': {'en': 5, 'rs': 12, 'd4': 26, 'd5': 19, 'd6': 13, 'd7': 6,
 notFound = True
 
 
+dev = InputDevice(config['keyboardDevice'])
+
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal, frame):
     global notFound
@@ -50,6 +53,10 @@ def hex_string_from_nfc_tag(tag):
 signal.signal(signal.SIGINT, end_read)
 
 GPIO.setwarnings(False)
+
+keypad_map = { 'KEY_KP0': 48, 'KEY_KP2': 50, 'KEY_KP3': 51,
+               'KEY_KP4': 52, 'KEY_KP5': 53, 'KEY_KP6': 54, 'KEY_KP7': 55,
+               'KEY_KP8': 56, 'KEY_KP9': 57 }
 
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
@@ -131,6 +138,55 @@ def add_match(match):
     socketIO.emit('addMatch', match.get_match_data())
     socketIO.wait(seconds=5)
 
+def clear_points_display():
+    # clear old points
+    for row in [1, 2]:
+       lcd.set_cursor(int(lcdConfig['cols'])-2, row)
+       lcd.write8(ord(' '), True)
+       lcd.write8(ord(' '), True)
+
+def wait_for_points(set, row):
+    # print current set
+    lcd.set_cursor(int(lcdConfig['cols'])-4, 0)
+    lcd.write8(ord('S'), True)
+    lcd.set_cursor(int(lcdConfig['cols'])-4, 1)
+    lcd.write8(ord(str(set)), True)
+    # clear old points in this row
+    lcd.set_cursor(int(lcdConfig['cols'])-2, row)
+    lcd.write8(ord(' '), True)
+    lcd.write8(ord(' '), True)
+    # reposition
+    lcd.set_cursor(int(lcdConfig['cols'])-2, row)
+    lcd.blink(True)
+    typed = 0
+    points = 0
+    # wait for keyboard input
+    for event in dev.read_loop():
+       if event.type == ecodes.EV_KEY:
+          key = categorize(event)
+          if key.keystate == KeyEvent.key_down:
+             if key.keycode == 'KEY_KP1':
+                if typed == 0:
+                   points += 10
+                else:
+                   points += 1
+                typed += 1   
+                print('writing 1 to row '+ str(row))
+                lcd.write8(ord('1'), True)
+             elif key.keycode in keypad_map:
+                if typed == 0:
+                   typed += 1
+                   lcd.write8(ord(' '), True)
+                mapped = keypad_map[key.keycode]
+                print('mapped {:d} --> {:s}'.format(mapped, chr(mapped)))
+                typed += 1
+                lcd.write8(mapped, True)
+                points += int(chr(mapped))
+          print('typed is now {:d} and points {:d}'.format(typed, points))
+          if typed == 2:
+             break
+    lcd.blink(False)
+    return points
 
 def clear_players():
     global players
@@ -161,11 +217,15 @@ while True:
     lcd.message('{}\n{}'.format(players[0].name, players[1].name))
     for x in (1, 2, 3):
         # loop for games to play
-        s = raw_input('Enter values for set {:d} (or X for end): '.format(x))
-        if s == 'X':
-            break
-        points = s.split(':')
-        match.add_game(Game(int(points[0]), int(points[1])))
+        #s = raw_input('Enter values for set {:d} (or X for end): '.format(x))
+        #if s == 'X':
+        #    break
+        #points = s.split(':')
+        home = wait_for_points(x, 0)
+        guest = wait_for_points(x, 1)
+        match.add_game(Game(int(home), int(guest)))
+        sleep(2)
+        clear_points_display()
 
     print('Match finished: ' + str(match.get_match_data()))
     add_match(match)
